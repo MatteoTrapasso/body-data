abfc8a540b7ffcb4414b575944f1eb11cbd40daa
mtrapass <matteo.trapasso97@gmail.com>
2021-04-23 11:19:15 +0200

    add users

--- src/app/main/models/vo/user.ts
+++ src/app/main/models/vo/user.ts
@@ -1,7 +1,9 @@
 export class User {
   public id: string = undefined;
-  public value: string = undefined;
   public name: string = undefined;
+  public surname: string = undefined;
+  public email: string = undefined;
+  public gender: string = undefined;
   public description: string = undefined;
   static selectId: (item: User) => string = item => item.id;
 }
